120826d8c8508ea4423222e18e243e284e3589de
Jéssica <160645996+jjessic@users.noreply.github.com>
2024-05-24 19:08:44 +0000

    Atualizando o código

--- script.js
+++ script.js
@@ -1,35 +1,120 @@
 const openModal = () => {
-    document.getElementById('modal').classList.add('active')
+    document.getElementById("modal").classList.add("active")
+
+    document.getElementById('title-modal').innerText = "Atualizar usuário"
+    document.getElementById('saveValue').innerText = "Atualizar";
 }
 
 const closeModal = () => {
-    document.getElementById('modal').classList.remove('active')
+    document.getElementById("modal").classList.remove("active")
 }
 
-document.getElementById('cadastrarUsuario').addEventListener('click', openModal);
+document.getElementById("cadastrarUsuario").addEventListener("click", openModal);
 
-document.getElementById('modalClose').addEventListener('click', closeModal);
 
-const cadastrarUsuario = () => {
+document.getElementById("modalClose").addEventListener("click", closeModal);
 
-    const nome = document.getElementById('nome').value
-    const email = document.getElementById('email').value
-    const celular = document.getElementById('celular').value
-    const cidade = document.getElementById('cidade').value
+const CreateNewUser = () => {
+    let listUser = []
 
+    const nome = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const celular = document.getElementById('celular').value;
+    const cidade = document.getElementById('cidade').value;
+    const id = Math.floor(Math.random() * 100);
+
+    const objUser = {
+        nomeUser: nome,
+        emailUser: email,
+        celularUser: celular,
+        cidadeUser: cidade,
+        idUser: id
+    }
+
+    if (localStorage.getItem('userInfos')) {
+        listUser = JSON.parse(localStorage.getItem('userInfos'));
+    }
+
+    listUser.push(objUser);
+
+    console.log(listUser);
+
+    localStorage.setItem('userInfos', JSON.stringify(listUser));
+
+    closeModal();
+
+    window.location.reload();
+};
+
+document.getElementById('saveValue').addEventListener('click', CreateNewUser);
+
+function carregarUsuario() {
+
+    if (localStorage.getItem('userInfos')) {
+        listUser = JSON.parse(localStorage.getItem("userInfos"));
+    }
+
+    if (listUser.length === 0) {
+        let tabela = document.getElementById("corpo-tabela");
+
+        tabela.innerHTML = `
+        <tr>
+            <td colspan="5"> Nenhum usuário cadastrado </td>
+        `
+    } else {
+        createTableUser(listUser)
+    }
 }
 
-//LÓGICA DE CADASTRO
+    document.addEventListener("DOMContentLoaded", carregarUsuario)
 
-//conseguir abrir o modal
-//inserir as informações, clicar no botão de salvar, salvar dados no localStorage
-//fechar o modal quando cadastrar OU cancelar
-//regarrego a pagina
+    function createTableUser(listUser) {
+        let tabela = document.getElementById("corpo-tabela");
+        
+        let template = "";
 
-//FUNÇÃO DE EXIBIR OS USUÁRIOS
+        listUser.forEach((user) => {
+            template += `
+            <tr>
+                <td>${user.nomeUser}</td>
+                <td>${user.emailUser}</td>
+                <td>${user.celularUser}</td>
+                <td>${user.cidadeUser}</td>
+                <td>                            
+                <button type="button" class="button green">Editar</button>
+                <button type="button" class="button red" onclick="deleteUser(${user.idUser})">Excluir</button>
+                </td>
+            </tr>
+            `;
+        });
+        tabela.innerHTML = template;
+    }
 
-//pegar os itens do localStorage (getItem)
-//colocar os dados dentro de um array
-//utilizar o método foreach para percorrer o array
-//criar a tabela usando o DOM (id do tbody)
+    function updateUser(id) {
+        openModal();
 
+        document.getElementById('title-modal').innerText = "Atualizar usuário"
+        document.getElementById('saveValue').innerText = "Atualizar";
+
+        const retornoData = JSON.parse(localStorage.getItem("userInfos"));
+
+        const usuarioEncontrado = retornoData.find
+    }
+
+    function deleteUser(id) {
+        alert(id)
+
+        let userList = JSON.parse(localStorage.getItem("userInfos")) || [];
+
+        const findIndex = userList.findIndex((userId) => userId.idUser == id);
+
+        console.log(findIndex);
+
+        if (findIndex !== -1) {
+            alert("Nenhum usuário encontrado");
+        } else {
+            userList.splice(findIndex, 1)
+            localStorage.setItem("userInfos", JSON.stringify(userList));
+            window.location.reload();
+        }
+        }
